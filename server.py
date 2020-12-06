8940fc2c510d4f2a7aa783c181b1112c27fa2e4c
David Lord <eternalcat@gmail.com>
2020-12-06 17:29:23 +1300

    Initial commit, stages AB and working on C

---
+++ server.py
@@ -0,0 +1,57 @@
+from typing import Tuple, Iterable
+
+from flask import Flask, render_template, request, make_response
+from pygments import highlight
+from pygments.lexers import guess_lexer
+from pygments.formatters import HtmlFormatter
+app = Flask(__name__)
+
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+# pygments stuff
+formatter = HtmlFormatter(linenos=False)
+
+
+def get_pygments_classes() -> Tuple[Iterable[str], Iterable[str]]:
+    """ A function to contain the ugly.
+    Get a list of pygments' possible CSS classes and turn them into
+        1) CSS selectors
+        2) names for variables to accompany them. """
+    pygments_css_classes = HtmlFormatter().get_token_style_defs('.highlight')
+    classes = [line[:line.find('{')].replace('.highlight ', 'span') for line in pygments_css_classes]
+    var_names = [line.replace('.', '. ', 1).replace('.', '-').replace(' ', '') for line in classes]
+    return classes, var_names
+
+pygments_css_classes, pygments_css_variables = get_pygments_classes()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def markup():
+    if request.method == 'POST':
+        code = request.form['code']
+        lexer = guess_lexer(code)
+        print(f"Choosing {lexer}")
+        result = highlight(code, lexer, formatter)
+        get_pygments_classes()
+        return render_template('show_twinkle.html', code=result)
+    else:
+        return render_template('form.html')
+
+
+@app.route('/smart_css.css')
+def smart_css():
+    rendered = render_template('twinkle_smart.css',
+                               # we must list() the zip() because it will be iterated over multiple times
+                               css_classes_and_variables=list(zip(pygments_css_classes, pygments_css_variables)))
+    response = make_response(rendered)
+    # set the content-type manually; by default it'll be text/html
+    response.headers['Content-Type'] = 'text/css'
+    return response
+
+
+@app.route('/smart_js.js')
+def smart_js():
+    rendered = render_template('twinkle_smart.js', css_variables=pygments_css_variables)
+    response = make_response(rendered)
+    response.headers['Content-Type'] = 'application/javascript'
+    return response
---
+++ templates/twinkle_smart.js
@@ -0,0 +1,45 @@
+const colors = ["#f6f3e8", "#656565", "#ffffff", "#f6f3e8", "#f6f3e8", "#343434", "#857b6f", "#384048", "#a0a8b0", "#f6f3e8", "#857b6f", "#e5786d", "#ddaa6f", "#ddaa6f", "#e5786d", "#99968b", "#e5786d", "#cae682", "#8ac6f2", "#95e454", "#92a65e", "#cae682", "#ccaa8f", "#8ac6f2", "#e5786d", "#f6f3e8", "#e7f6da", "#95e454", "#95e454", "#cae682", "#cae682", "#ccaa8f", "#ccaa8f", "#99968b", "#99968b", "#cae682", "#cae682", "#99968b", "#95e454", "#95e454", "#cae682", "#cae682", "#ccaa8f", "#ccaa8f", "#99968b", "#8ac6f2", "#95e454", "#cae682", "#8ac6f2", "#cae682", "#8ac6f2", "#95e454", "#95e454", "#cae682", "#cae682", "#99968b", "#e5786d", "#e5786d", "#95e454", "#2cae682", "#8ac6f2", "#ccaa8f", "#f6f3e8"];
+const types = [
+    {% for css_var in css_variables %}
+    "{{ css_var }}",
+    {% endfor %}
+    'unknown' // special case: a <span> with no class or an un-recognised class
+];
+
+let timer;
+
+function toggle() {
+    const the_button = document.getElementById("the-button");
+    if (timer === undefined) {
+        timer = setInterval(change, 500);
+        the_button.textContent = "Cease";
+        change(); // do it right now, rather than waiting 500ms for the first one
+    } else {
+        clearTimeout(timer);
+        timer = undefined;
+        the_button.textContent = "Begin"
+    }
+}
+
+
+function change() {
+    colors_this_iteration = colors.slice();
+    types.forEach(type => {
+        // if we've run out of colours, start again
+        if (colors_this_iteration.length < 1) {
+            colors_this_iteration = colors.slice();
+        }
+        // randomly pick a colour, use it, and remove it from the array
+        index = Math.floor(Math.random() * colors_this_iteration.length);
+        let next_color = colors_this_iteration.splice(index, 1)[0];
+        document.documentElement.style.setProperty('--' + type, next_color);
+    })
+}
+
+
+function transitionOff() {
+    // span { transition: unset; }
+}
+function transitionOn() {
+    // span { transition: all 0.5s; }
+}
